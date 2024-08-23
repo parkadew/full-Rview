92fc1d854827dcf0ebb893456d7a82e3933a558f
parkadew <pkm_design@naver.com>
2024-08-23 16:34:45 +0900

    8/23

--- function.js
+++ function.js
@@ -9,37 +9,53 @@
 // console.log(a)
 
 let arr = [];
+let isStarted = false;
 
 let RandomNum = () => {
-    arr = [];
 
-    const RandomArr = () => {
-        for (let i = 0; i < 6; i++) {
-            let random = Math.floor(Math.random() * 10)
-            arr.push(random)
-        } return arr
+
+    if (isStarted === false) {
+        // 타이머가 작동중이 아닐떄??
+        isStarted === true
+
+
+        arr = [];
+
+        const RandomArr = () => {
+            for (let i = 0; i < 6; i++) {
+                let random = Math.floor(Math.random() * 10)
+                arr.push(random)
+            } return arr
+        }
+        RandomArr()
+        console.log(arr)
+
+        let StringNumber = arr.join('')
+        console.log(StringNumber)
+
+        document.getElementById("target").innerText = StringNumber
+        document.getElementById("target").style.color = "#" + StringNumber
+
+        let time = 10;
+
+        setInterval(() => {
+            if (time >= 0) {
+                const min = Math.floor(time / 60)
+                let sec = String(time % 60).padStart(2, "0")
+                document.getElementById('timer').innerText = min + ":" + sec
+                time = time - 1
+            }
+            else {
+                document.getElementById('finish').disabled = true
+                isStarted === false;
+            }
+        }, 1000);
+
+    } else {
+        // 타이머가 작동중일떄
     }
-    RandomArr()
-    console.log(arr)
 
-    let StringNumber = arr.join('')
-    console.log(StringNumber)
 
-    document.getElementById("target").innerText = StringNumber
-    document.getElementById("target").style.color = "#" + StringNumber
 
-    let time = 5;
-
-    setInterval(() => {
-        if (time >= 0) {
-            const min = Math.floor(time / 60)
-            let sec = String(time % 60).padStart(2, "0")
-            document.getElementById('timer').innerText = min + ":" + sec
-            time = time - 1
-        }
-        else {
-            document.getElementById('finish').disabled = true
-        }
-    }, 1000);
 
 }
